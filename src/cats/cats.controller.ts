63f87c14569f37fc73be471e8a62219408984e86
kidman90 <kacprzak90@gmail.com>
2018-04-27 14:47:26 +0200

    Guards

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,14 +1,18 @@
-import { Controller, Get, HttpCode, Post, Body, Param, HttpException, HttpStatus, ForbiddenException, UseFilters } from '@nestjs/common';
+import { Controller, Get, HttpCode, Post, Body, Param, HttpException, HttpStatus, ForbiddenException, UseFilters, UseGuards, ReflectMetadata } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 import { CatsService } from './cats.service';
 import { Cat } from './interfaces/cat.interface';
 import { ParseIntPipe } from '../common/pipes/parse-int.pipe';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { Roles } from '../common/decorators/roles.decorator';
 
 @Controller('cats')
+@UseGuards(RolesGuard)
 export class CatsController {
   constructor(private readonly catsService: CatsService) { }
 
   @Post()
+  @Roles('admin')
   async create(@Body() createCatDto: CreateCatDto) {
     this.catsService.create(createCatDto);
     // throw new ForbiddenException();
--- src/common/middlewares/logger.middleware.ts
+++ src/common/middlewares/logger.middleware.ts
@@ -4,6 +4,7 @@ import { Middleware, NestMiddleware, ExpressMiddleware } from '@nestjs/common';
 export class LoggerMiddleware implements NestMiddleware {
   resolve(name: string): ExpressMiddleware {
     return (req, res, next) => {
+      req.user = { roles: ['admin'] };
       console.log(`[${name}] Request...`); // [ApplicationModule] Request...
       next();
     };
